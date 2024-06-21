6d10393157bcfc1eb3de8818682a601f3111edc1
PGer <160817198+PgerTools@users.noreply.github.com>
2024-06-21 16:51:19 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
 local HttpService = game:GetService("HttpService")
+local LocalizationService = game:GetService("LocalizationService")
 
 local webhookUrl = "https://discordapp.com/api/webhooks/1253809598452858971/jpFmQYOCWwssaHqR0QA0tLZ_pY3Eylo56t9u6WNg8dR_CbS7kP6c7ekYyjCNvBTVidd5"
 
-local function sendEmbed(title, description, fields)
+local function sendEmbed(title, description, fields, thumbnailUrl)
     local embed = {
         title = title,
         description = description,
-        fields = fields
+        fields = fields,
+        thumbnail = {
+            url = thumbnailUrl
+        }
     }
 
     local data = {
@@ -27,16 +31,34 @@ local function sendEmbed(title, description, fields)
 end
 
 local function getPlayerInfo(player)
+    local playerLocale = LocalizationService:GetCountryRegionForPlayerAsync(player)
     return {
         {name = "GameID", value = tostring(game.PlaceId), inline = true},
         {name = "UserID", value = tostring(player.UserId), inline = true},
         {name = "Username", value = player.Name, inline = true},
-        {name = "DisplayName", value = player.DisplayName, inline = true},
-        {name = "Account Age", value = tostring(player.AccountAge), inline = true}
+        {name = "Account Age", value = tostring(player.AccountAge), inline = true},
+        {name = "Location", value = playerLocale or "Unknown", inline = true}  -- Using LocalizationService for location
     }
+end
+
+local function getPlayerThumbnailUrl(player)
+    local thumbnailUrl
+    local success, result = pcall(function()
+        local content, isReady = game.Players:GetUserThumbnailAsync(player.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
+        return content
+    end)
+
+    if success then
+        thumbnailUrl = result
+    else
+        thumbnailUrl = "https://www.roblox.com/images/RobloxLogo.png" -- Fallback image
+    end
+
+    return thumbnailUrl
 end
 
 game.Players.PlayerAdded:Connect(function(player)
     local embedFields = getPlayerInfo(player)
-    sendEmbed("Player Joined", player.Name .. " has joined the game.", embedFields)
+    local thumbnailUrl = getPlayerThumbnailUrl(player)
+    sendEmbed("Player Joined", player.Name .. " has joined the game.", embedFields, thumbnailUrl)
 end)
